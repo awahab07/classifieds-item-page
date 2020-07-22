152de3e5d765161df3438377d6db30994e5fcf68
Abdul Zahid <awahab07@yahoo.com>
2020-07-22 23:55:19 +0200

    Updates

---
+++ precache-manifest.a60ec5accb95004e0dd12282a99a8e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a9c85339d277c3d7e7e597b0ea785b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9c5cac434187fef19bb",
+    "url": "/static/js/2.379ac017.chunk.js"
+  },
+  {
+    "revision": "86f3d4f309858e3852400d83b631a76b",
+    "url": "/static/js/2.379ac017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d78bc31a33fe054e0c",
+    "url": "/static/js/main.a5a7a9aa.chunk.js"
+  },
+  {
+    "revision": "33b9d1d3ab095b774770",
+    "url": "/static/js/runtime-main.c4b82a39.js"
+  }
+]);
